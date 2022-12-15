f13254358406bc331ce2dd7371eb28706e3dbdbd
Orejarena <jmoc951@gmail.com>
2022-12-15 16:08:26 -0500

    Todo

--- src/views/registerViews/registerSelector/registerSelector.js
+++ src/views/registerViews/registerSelector/registerSelector.js
@@ -2,8 +2,6 @@ import { partCreator } from "../../../components/applicantPart/applicantPart.js"
 
 const app = document.querySelector('#app')
 
-const applicantPart = partCreator('https://i.ibb.co/PZ5Npww/imgAppl.png','Como aspirante','Lorem ipsum is simply dummy text of the printing and typesetting industry.')
-
-app.appendChild(applicantPart)
+app.appendChild(partCreator('https://i.ibb.co/PZ5Npww/imgAppl.png','Como aspirante','Lorem ipsum is simply dummy text of the printing and typesetting industry.'))
 /* 
 app.appendChild(totalSection) */
