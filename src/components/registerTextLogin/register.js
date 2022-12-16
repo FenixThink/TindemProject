b543b6d7c675d0a30007e069cda96a3f9907eb57
nessa26 <vanessa.coex@gmail.com>
2022-12-16 16:25:02 -0500

    new changes modification of files login.html, login.js and modification of registerTextLogin component

--- src/components/registerTextLogin/register.js
+++ src/components/registerTextLogin/register.js
@@ -1,15 +1,21 @@
-import { registerTextLogin } from '../registerTextLogin/register.js';
-
 export const registerTextLogin = () => {
-    const text = document.createElement('span');
-    const link = document.createElement('a');
-    text.textContent = `¿No tienes cuenta?`;
-    link.textContent = `Registrarse`;
-    Text.className = "text";
-    Text.className = "register";
+    //Creación del input del span del texto
+    const text = document.createElement('span')
+    text.textContent="¿No tienes cuenta? "
+    text.style.color = "gray"
+
+    //Creación del link para registrarse
+    const a = document.createElement('a');
+    const linkText = document.createTextNode("Registrarse");
+    a.appendChild(linkText);
+    a.href = "#";
+    a.style.color = "black";
+    a.style.fontweight = "bolder";
     
-    return text
-    
+    //Creación del div donde se encuentra el texto y el link
+    const parent = document.createElement('div')
+    parent.appendChild(text)
+    parent.appendChild(a)
+    return parent;
 }
 
-  
