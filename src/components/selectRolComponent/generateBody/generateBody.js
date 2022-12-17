8f1692f5d01db86c63543753619ab320bda8e90c
FenixThink <brayanxdvanegas@hotmail.com>
2022-12-17 09:16:27 -0500

    Agregando selecionar rol

---
+++ src/components/selectRolComponent/generateBody/generateBody.js
@@ -0,0 +1,17 @@
+import { generateCard } from "../generateCard/generateCard.js"
+export const generateBody = ()=>{
+    const modal = document.createElement("div")
+    modal.id = "modal"
+    modal.classList = "appModal"
+    
+    const appModalContentmodal = document.createElement("div")
+    appModalContentmodal.classList = "appModalContent-modal"
+
+    const cardAspirante = generateCard("/svg/aspirante.svg","imgAspirante","Como Aspirante","Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium laboriosam vero repellendus ad nam incidunt natus optio eligendi","Continuar","../registerViews/registerAplicants/index.html")
+
+    const cardEmpresa = generateCard("/svg/empresa.svg","imgEmpresa","Como Empresa","Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium laboriosam vero repellendus ad nam incidunt natus optio eligendi","Continuar","../registerViews/registerBusiness/log.html")
+    modal.appendChild(appModalContentmodal)
+    appModalContentmodal.appendChild(cardAspirante)
+    appModalContentmodal.appendChild(cardEmpresa)
+    return modal
+}
