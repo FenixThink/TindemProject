76482d7a77b30e39a5ca1c8474b057beae90cdea
Orejarena <jmoc951@gmail.com>
2022-12-17 23:23:02 -0500

    renombrado de ciertaa funcion y tal

--- src/components/modalUploadPhotoCompany/functionButtonUploadImage.js
+++ src/components/modalUploadPhotoCompany/functionButtonUploadImage.js
@@ -6,6 +6,7 @@ export function miFunction() {
     const modalUploadImage = modalUpload()
     const divFatherUpload = document.createElement('div')
     divFatherUpload.className='divFatherUpload'
+    divFatherUpload.style.display="flex"
     divFatherUpload.appendChild(modalUploadImage)
 
     return divFatherUpload;
