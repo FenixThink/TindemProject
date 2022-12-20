528b7a5ac637ad39a8f89c4108fff3426e261be2
Orejarena <jmoc951@gmail.com>
2022-12-20 08:27:30 -0500

    Validacion de datos del login

--- src/components/login/button.js
+++ src/components/login/button.js
@@ -1,11 +1,14 @@
 export const buttonLogin = () =>{
 
     const button = document.createElement('button')
+    button.id = "sendBtn"
     button.textContent="Iniciar Sesión"
+    button.type = "submit"
+    button.style.cursor = "pointer"
     button.classList.add("button")
     const div = document.createElement('div')
     div.classList.add("button-div");
     div.appendChild(button)
 
-    return div
+    return button
 }
--- src/components/login/loginCard.js
+++ src/components/login/loginCard.js
@@ -6,7 +6,9 @@ import { buttonLogin } from "../../components/login/button.js";
 
 export const loginCard = () => {
 
-    const totalCard = document.createElement('div')
+    const totalCard = document.createElement('form')
+    totalCard.action = ""
+    totalCard.method = "POST" 
     totalCard.classList.add("total-card");
 
     totalCard.appendChild(tittleLogin());
