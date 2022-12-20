b0efd9758ddc04444b010fb1f09c6d6073c8fdc1
nathalia258 <silvianathaliahernandezsandova@gmail.com>
2022-12-19 23:39:25 -0500

    Componente area de intereses - main

--- src/views/mainView/empresa/main.js
+++ src/views/mainView/empresa/main.js
@@ -1,5 +1,7 @@
 import { upperComponents } from "../../../components/mainViewComponents/componentA/componentA.js"
+import { interests } from "../../../components/mainViewComponents/componentA/componentB.js"
 
 const app = document.querySelector('#app')
 
 app.appendChild(upperComponents("Nombre de empresa"," nit: ","Descripcion de empresa: "))
+app.appendChild(interests())
