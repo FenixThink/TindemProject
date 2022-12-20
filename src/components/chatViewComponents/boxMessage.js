9d66f7ace9f453dc2e0bc4bdcfb7a350d65d7e23
mpauher <mpaulah24@gmail.com>
2022-12-19 23:28:59 -0500

    Componente de cajas de texto del chat

---
+++ src/components/chatViewComponents/boxMessage.js
@@ -0,0 +1,26 @@
+export function boxMessage(color){
+
+    // Creación del p que contiene el mensaje
+    const p = document.createElement("p")
+    p.className="message"
+    const message = document.createTextNode("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore magni eaque dolore reiciendis iusto asperiores fugiat vitae sed. Nesciunt eligendi repudiandae sit temporibus deleniti a est, id molestias ipsam ipsa doloribus quo ad labore, velit vel quaerat voluptates dolores ut tempore odit? Explicabo, ipsa? Voluptatum ducimus harum distinctio. Voluptatibus, nobis.");
+    p.appendChild(message)
+
+    //Creación del p que contiene la hora
+    const p1 = document.createElement("p")
+    p1.className="hour"
+    const hour = document.createTextNode("12:20 PM");
+    p1.appendChild(hour)
+
+    // Creación del div del mensaje
+    const div = document.createElement('div')
+    div.classList.add("box-message-"+color);
+    div.appendChild(p)
+
+    // Creción del div padre
+    const parent = document.createElement('div')
+    parent.appendChild(div)
+    parent.appendChild(p1)
+
+    return parent
+}
