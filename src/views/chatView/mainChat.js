6a1c509977eb4fdbf417afe5683d1ed9de6abbe3
mpauher <mpaulah24@gmail.com>
2022-12-19 23:30:15 -0500

    Añadir componente de mensaje a vista y css

--- src/views/chatView/mainChat.js
+++ src/views/chatView/mainChat.js
@@ -1,3 +1,10 @@
-document.querySelector(".dart").addEventListener("click",()=>{
-    window.location = "../mainView/Aspirante/mainAspirante.html"
-})
+import { boxMessage } from "../../components/chatViewComponents/boxMessage.js";
+
+const divApp = boxMessage("gris")
+
+document.querySelector('#app').appendChild(divApp);
+
+
+// document.querySelector(".dart").addEventListener("click",()=>{
+//     window.location = "../mainView/Aspirante/mainAspirante.html"
+// })
