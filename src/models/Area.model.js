62977a0da76b574422d4a7b41a9fe87cded85e1c
Ansear <sebastianarciniegas2003@gmail.com>
2023-02-03 14:15:23 -0500

    Agregando set y get en modelos Area y Match

--- src/models/Area.model.js
+++ src/models/Area.model.js
@@ -5,6 +5,9 @@ class Area{
         this.name = body.name
     }
 
+    set Name(name){ this.name = name }
+    
+    get Name(){ return this.name }
 }
 
 export default Area
--- src/models/Match.model.js
+++ src/models/Match.model.js
@@ -1,10 +1,15 @@
 class Match{
 
     constructor(body){
-        this.id_Applicant = body.id_Applicant,
-        this.id_Company = body.id_Company
+        this.#id_Applicant = body.id_Applicant,
+        this.#id_Company = body.id_Company
     }
 
+    set Id_Applicant(id_applicant){ this.#id_Applicant = id_applicant }
+    set Id_Company(id_company){ this.#id_Company = id_company }
+
+    get Id_Applicant(){ return this.#id_Applicant }
+    get Id_Company(){ return this.#id_Company }
 }
 
 export default Match
