8ca104190e119829d25799335e0be90e22b29cd3
paoGelvez <pgelvezandrea@gmail.com>
2023-02-03 10:29:48 -0500

    modelo de profile

---
+++ src/models/Profile.models.js
@@ -0,0 +1,15 @@
+class Profile{
+    
+    constructor(name,description,interests,iLikelt,UseId,CitId){
+        this.name = name;
+        this.description = description;
+        this.interests = interests;
+        this.iLikelt = iLikelt;
+        this.UseId = UseId;
+        this.CitId = CitId
+    }
+
+    
+}
+
+export default Profile;
