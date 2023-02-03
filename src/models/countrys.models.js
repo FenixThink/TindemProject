5a7a533c4b35046430f2db17923388278b4fad02
00Aids <johanarley352@gmail.com>
2023-02-03 14:16:27 -0500

    set y get del modelo country

--- src/models/countrys.models.js
+++ src/models/countrys.models.js
@@ -1,7 +1,11 @@
 class CountryModel{
     constructor(name){
-        this.name = name;
+        this.#name = name;
     }
+
+    set name(name){ this.#name = name; }
+
+    get name(){ return this.#name; }
 }
 
 export default CountryModel;
