d08cdca7dc4662b15ed574a42761b2b4702bd7c0
nathalia258 <silvianathaliahernandezsandova@gmail.com>
2023-02-03 16:41:10 -0500

    Company Model

---
+++ src/models/company.models.js
@@ -0,0 +1,18 @@
+class CompanyModel{
+
+    constructor(body){
+        this.#NIT = body.NIT,
+        this.#dayOfFounded = body.dayOfFounded
+        this.#id_Profile = body.id_Profile
+    }
+
+    set NIT(NIT){ this.#NIT = NIT }
+    set dayOfFounded(dayOfFounded){ this.#dayOfFounded = dayOfFounded }
+    set id_Profile(id_Profile){ this.#id_Profile = id_Profile }
+
+    get NIT(){ return this.#NIT }
+    get dayOfFounded(){ return this.#dayOfFounded }
+    get id_Profile(){ return this.#id_Profile }
+}
+
+export default CompanyModel
