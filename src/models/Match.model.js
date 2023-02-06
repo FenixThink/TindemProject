5b27221ff814470effc2070cf3319731b8926d6f
Carlos Felipe Romero Ramírez <carlospipe1010@gmail.com>
2023-02-06 21:42:54 +0000

    metodo CreateMatch en el aclase Match

--- src/models/Match.model.js
+++ src/models/Match.model.js
@@ -1,17 +1,25 @@
-class Match {
+import { pool } from "../../db/db.js"
 
-    static table = 'ServiceMatch'
-
-    constructor(body) {
-        this.#id_Applicant = body.id_Applicant,
-            this.#id_Company = body.id_Company
+export class Match {
+    //insertar las relaciones entre los aplicantes y las empresas 
+    CreateMatch = async (req, res)=>{
+        const {id_applicant, id_company} = req.body
+        const [rows] = await pool.query('INSERT INTO servicematch (id_applicant, id_company) VALUES (?, ?)', [id_applicant, id_company])
+        return 
     }
 
-    set Id_Applicant(id_applicant) { this.#id_Applicant = id_applicant }
-    set Id_Company(id_company) { this.#id_Company = id_company }
+    // static table = 'ServiceMatch'
 
-    get Id_Applicant() { return this.#id_Applicant }
-    get Id_Company() { return this.#id_Company }
+    // constructor(body) {
+    //     this.#id_Applicant = body.id_Applicant,
+    //         this.#id_Company = body.id_Company
+    // }
+
+    // set Id_Applicant(id_applicant) { this.#id_Applicant = id_applicant }
+    // set Id_Company(id_company) { this.#id_Company = id_company }
+
+    // get Id_Applicant() { return this.#id_Applicant }
+    // get Id_Company() { return this.#id_Company }
 }
 
 export default Match
