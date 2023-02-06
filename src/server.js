20eb34a6943f604910c9389825ed82149db4acdf
Carlos Felipe Romero Ramírez <carlospipe1010@gmail.com>
2023-02-06 21:47:07 +0000

    importe la ruta del Match

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoute from './routes/user.routes.js'
+import routerMatch from './routes/Match.routes.js'
 
 const app = express();
 app.use(express.json());
 app.use(userRoute)
+app.use(routerMatch)
 app.use(express.static('./'))
 
 app.listen(5173, ()=>{
