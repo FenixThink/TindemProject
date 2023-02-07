0b13c927839c4067af115aaf1001bb82a37298d7
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-02-07 17:09:54 -0500

    Company controller, model, route

--- src/controllers/Company.controller.js
+++ src/controllers/Company.controller.js
@@ -1,25 +1,20 @@
 import Company from "../models/Company.model.js";
 
 class CompanyController {
-    
-    static companyRegister = (req,res) =>{
-        res.render('../views/registerViews/registerBusiness/index.html')
-    };
 
-    static Create(){
+    static create(){
         return  async (req,res)=>{
-            const company = new Company(req.body)
             try {
-               const data = await company.Create()
-               res.status(200).json({message: 'Company successfully created'})
+                const company = new Company(req.body)
+                const data = await company.Create()
+                res.status(200).json({message: 'Company successfully created'})
             } catch (error) {
                 res.status(500).json({
-                    Message : error.Messages 
-                })
+                    message: error.message
+                });
             }
         }
     }
-
 
 }
 
