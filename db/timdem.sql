bfd6b4940f0e85c6b7bbf1d3daf043840fb964c1
KDOE-117 <jdn2037@gmail.com>
2023-02-06 22:11:36 -0500

    Estructura BD está sí 100% seguro.

--- db/timdem.sql
+++ db/timdem.sql
@@ -1,87 +1,74 @@
 DROP DATABASE IF EXISTS Tindem;
 CREATE DATABASE IF NOT EXISTS Tindem;
+use Tindem;
 
-USE Tindem;
-
-DROP TABLE IF EXISTS UserSpecialization; 
-DROP TABLE IF EXISTS ServiceMatch;
-DROP TABLE IF EXISTS Applicant;
-DROP TABLE IF EXISTS ProfileAccount;
-DROP TABLE IF EXISTS City;
-DROP TABLE IF EXISTS Country;
-DROP TABLE IF EXISTS Company;
-DROP TABLE IF EXISTS UserAccount;
-DROP TABLE IF EXISTS Career;
-DROP TABLE IF EXISTS Specialization;
-
-CREATE TABLE InterestArea (
-    id_interest INT(11) PRIMARY KEY,
-    name VARCHAR(60) NOT NULL
+create table  country (
+	id int(11) primary key auto_increment,
+    name varchar(60) not null unique
 );
 
-CREATE TABLE UserAccount (
-	id_user INT(11) AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(40) NOT NULL UNIQUE,
-    userPassword VARCHAR(30) NOT NULL UNIQUE
+create table interest_area(
+	id int(11) primary key auto_increment,
+    name varchar(60) not null
 );
 
-CREATE TABLE SpecificInterest (
-    id_specialization INT(11) PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    id_interest INT(11) NOT NULL,
-    CONSTRAINT FK_Interest_Area FOREIGN KEY (id_interest) REFERENCES InterestArea (id_interest) ON DELETE NO ACTION ON UPDATE CASCADE
+create table user(
+	id int(11) primary key auto_increment,
+    email varchar(60) not null,
+    password varchar(30) not null
 );
 
-CREATE TABLE Company(
-	id_company INT(11) AUTO_INCREMENT PRIMARY KEY,
-	nit INT(11) NOT NULL,
-    dayOfFounded DATE NOT NULL
+create table city (
+	id int(11) primary key auto_increment,
+    name varchar(60) not null,
+    id_country int(11) not null,
+    constraint fk_id_country foreign key (id_country) references country(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-CREATE TABLE Country(
-	id_country INT(11) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(40) NOT NULL UNIQUE
+create table specific_interest (
+	id int(11) primary key auto_increment,
+    name varchar(60) not null,
+    id_interest int(11) not null,
+    constraint fk_id_interest foreign key (id_interest) references interest_area(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-CREATE TABLE City(
-	id_city INT(11) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(85) NOT NULL,
-    id_country INT(11) NOT NULL,
-    CONSTRAINT FK_Country_Id FOREIGN KEY(id_country) REFERENCES Country(id_country) ON DELETE NO ACTION ON UPDATE CASCADE
+create table company (
+	id int(11) primary key auto_increment,
+    company_name varchar(30) not null,
+    company_username varchar(30) not null,
+    company_fundation date not null,
+    company_description varchar(255) not null,
+    id_user INT(11) not null,
+    id_city INT(11) not null,
+    id_interest INT(11) not null,
+    constraint fk_id_user foreign key (id_user) references user(id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    constraint fk_id_city foreign key (id_city) references city(id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    constraint fk_id_interest2 foreign key (id_interest) references specific_interest(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-CREATE TABLE ProfileAccount(
-	id_profile INT(11) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    profileDescription VARCHAR(255) NULL,
-    id_company INT(11) NOT NULL,
-    id_user INT(11) NOT NULL,
-    id_city INT(11) NOT NULL,
-    id_career INT(11) NOT NULL,
-    CONSTRAINT FK_Company_Id FOREIGN KEY(id_company) REFERENCES Company(id_company) ON DELETE NO ACTION ON UPDATE CASCADE,
-    CONSTRAINT FK_UserAccount_Id FOREIGN KEY(id_user) REFERENCES UserAccount(id_user) ON DELETE NO ACTION ON UPDATE CASCADE,
-    CONSTRAINT FK_City_Id FOREIGN KEY(id_city) REFERENCES City(id_city) ON DELETE NO ACTION ON UPDATE CASCADE
+create table applicant (
+	id int(11) primary key auto_increment,
+    applicant_name varchar(30) not null,
+    applicant_lastname varchar(30) not null,
+    applicant_birthdate date not null,
+    applicant_description varchar(255) not null,
+    applicant_gender enum('masculino', 'femenino') not null,
+    id_user int(11) not null,
+    id_city int(11) not null,
+    id_interest  int(11) not null,
+    constraint fk_id_user2 foreign key (id_user) references user(id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    constraint fk_id_city2 foreign key (id_city) references city(id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    constraint fk_id_interest3 foreign key (id_interest) references specific_interest(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-CREATE TABLE Applicant(
-	id_applicant  INT AUTO_INCREMENT PRIMARY KEY,
-    dateOfBirth DATE NOT NULL,
-    id_profile INT(11),
-    CONSTRAINT FK_Profile_Id2 FOREIGN KEY(id_profile) REFERENCES ProfileAccount(id_profile) ON DELETE NO ACTION ON UPDATE CASCADE
-);
-
-CREATE TABLE ServiceMatch (
-	id_match INT AUTO_INCREMENT PRIMARY KEY,
-    id_applicant INT(11) NOT NULL,
-    id_company INT(11) NOT NULL,
-    CONSTRAINT FK_Applicant_Id FOREIGN KEY(id_applicant) REFERENCES Applicant(id_applicant) ON DELETE NO ACTION ON UPDATE CASCADE,
-    CONSTRAINT FK_Company_Id2 FOREIGN KEY(id_company) REFERENCES Company(id_company) ON DELETE NO ACTION ON UPDATE CASCADE
-);
-
-CREATE TABLE UserSpecialization (
-    id_user_specialization INT(11) PRIMARY KEY,
-    id_profile  INT(11) NOT NULL,
-    id_specialization INT(11) NOT NULL,
-    CONSTRAINT PK_Profile_id3 FOREIGN KEY (id_profile)REFERENCES ProfileAccount(id_profile) ON DELETE NO ACTION ON UPDATE CASCADE,
-    CONSTRAINT PK_Id_Specialization FOREIGN KEY (id_specialization)REFERENCES SpecificInterest(id_specialization) ON DELETE NO ACTION ON UPDATE CASCADE
+create table actions (
+	id int(11) primary key auto_increment,
+    action_time datetime not null,
+    action enum('like','dislike'),
+    action_autor enum('aspirante','empresa'),
+    action_match bit not null, 
+    id_applicant int(11) not null,
+    id_company int(11) not null,
+    constraint fk_id_applicant foreign key (id_applicant) references applicant(id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    constraint fk_id_company foreign key (id_company) references company(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
