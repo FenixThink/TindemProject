3f81937186eaa02d63cc20d3b8126990a84e70e9
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-02-07 17:36:59 -0500

    Routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import userRoute from './routes/user.routes.js'
 import routerMatch from './routes/Match.routes.js'
-import routerCity from './routes/city.route.js';
-import routerArea from './routes/Area.routes.js';
+import routerCity from './routes/city.routes.js';
+import routerArea from './routes/area.routes.js';
+import profileAccountRoutes from './routes/profileAccount.routes.js';
+import companyRoutes from './routes/company.routes.js';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,9 @@ app.use(userRoute)
 app.use(routerMatch)
 app.use(routerCity)
 app.use(routerArea)
+app.use(profileAccountRoutes)
+app.use(companyRoutes)
+
 app.use(express.static('./'))
 app.listen(5173, ()=>{
     
