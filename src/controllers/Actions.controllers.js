758f1e87ddd48c1e5416f9547f720fc6bd846ecc
Carlos Felipe Romero Ramírez <carlospipe1010@gmail.com>
2023-02-07 20:59:52 +0000

    Creación del archivo Actions.controllers.js con el método ActionCreate para el manejo de errores

---
+++ src/controllers/Actions.controllers.js
@@ -0,0 +1,17 @@
+import Actions from "../models/Actions.model.js";
+
+const Action = new Actions()
+
+class ActionController {
+    //control de errores con try catch
+    ActionCreate = async (req, res)=>{
+        try {
+            const result = await Action.CreateMatch(req, res)
+            res.send("success full")
+        } catch (error) {
+            res.send(error.message)
+        }
+    }
+}
+
+export default ActionController
