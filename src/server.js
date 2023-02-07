6b413b9b61c171a7255dabdcd0ea6b8ae8a08c96
yuliethCor08 <yuliethcorrea08@gmail.com>
2023-02-07 17:32:30 -0500

    arreglo de rutas

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import userRoute from './routes/user.routes.js'
 import routerAction from './routes/Actions.routes.js'
 import routerCity from './routes/city.route.js';
 import routerArea from './routes/Area.routes.js';
+import routerProfileSpecialization from './routes/Profile_Specialization.routes.js';
+import routerArea from './routes/area.routes.js';
+import routeApplicant from './routes/registerAplicant.routes.js';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +13,8 @@ app.use(userRoute)
 app.use(routerAction)
 app.use(routerCity)
 app.use(routerArea)
+app.use(routerProfileSpecialization)
+app.use(routeApplicant)
 app.use(express.static('./'))
 app.listen(5173, ()=>{
     
