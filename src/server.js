c32fabc266d88b388dfd5cea7e0800fa499031b6
Luis <lorejarena2@hotmail.com>
2023-02-07 15:39:36 -0500

    Creacion del ActionModel, ActionController y ActionRoutes ademas importe el ActionRoutes en el archivo server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import userRoute from './routes/user.routes.js'
-import routerMatch from './routes/Match.routes.js'
+import routerAction from './routes/Actions.routes.js'
 import routerCity from './routes/city.route.js';
-import routerArea from './routes/Area.routes.js';
+//import routerArea from './routes/Area.routes.js';
 
 const app = express();
 app.use(express.json());
 app.use(userRoute)
-app.use(routerMatch)
+app.use(routerAction)
 app.use(routerCity)
-app.use(routerArea)
+//app.use(routerArea)
 app.use(express.static('./'))
 app.listen(5173, ()=>{
     
