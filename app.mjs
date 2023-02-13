c0f5c271363cb54fd5a51bd583c192682b3be675
paoGelvez <pgelvezandrea@gmail.com>
2023-02-13 15:07:16 -0500

    retoques en la ruta de chats

--- app.mjs
+++ app.mjs
@@ -12,6 +12,7 @@ import indexRouter from './routes/index.routes.js'
 import userRouter from './routes/user.routes.js'
 import applicantRouter from './routes/applicant.routes.js'
 import companyRouter from './routes/company.routes.js'
+import chatRoutes from './routes/chats.routes.js'
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use(userRouter);
 app.use(applicantRouter);
 app.use(companyRouter);
+app.use(chatRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
