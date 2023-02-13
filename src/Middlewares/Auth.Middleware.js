0f473ec87833da7b8d166aad094e86102ccf2689
KDOE-117 <jdn2037@gmail.com>
2023-02-13 17:59:29 -0500

    Image issues with mongo Coex

--- src/Middlewares/Auth.Middleware.js
+++ src/Middlewares/Auth.Middleware.js
@@ -1,12 +1,16 @@
 import jwt from "jsonwebtoken";
+
+/*Dependencias para gestionar la base de datos con mongo*/
+import multer from "multer";
+
 class Middelwares {
-    Authmidelware = (req,res,next)=>{
-        const accessToken =  req.headers['autorization'] || req.query.accessToken;
+    Authmidelware = (req, res, next) => {
+        const accessToken = req.headers['autorization'] || req.query.accessToken;
         if (!accessToken) res.send('Access Denied');
-        jwt.verify(accessToken,process.env.SECRET,(err,user)=>{
-            if (err){
+        jwt.verify(accessToken, process.env.SECRET, (err, user) => {
+            if (err) {
                 res.send('access denied, token expired or incorrect')
-            }else{
+            } else {
                 req.user = user;
                 next();
             }
