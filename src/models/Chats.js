6553c5693209dc727e84cc7af959c6f0353f7461
Orejarena <jmoc951@gmail.com>
2023-02-13 12:06:20 -0500

    Reconfiguracion de la lista de people para buena simulacion del response

--- src/models/Chats.js
+++ src/models/Chats.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-
 //esquema
 // estructura de datos que va a contener el modelo
 const ChatsSchema = new mongoose.Schema({
@@ -11,23 +10,8 @@ const ChatsSchema = new mongoose.Schema({
     },
     message : {
         type:Array
-        // type:String
-        
     }
-
-
 })
 
-//modelo
-
-
-
-// Chats.create(
-//     {
-//         idApplicant:1,
-//         idCompany:2,
-//         message:[{text:"oola",role:'receiver',hour:"15:00"},{text:" hqoola",role:'transmitter',hour:"15:01"}]
-//     }
-// )
 
 export default  mongoose.model('chats',ChatsSchema)
