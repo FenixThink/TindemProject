84559514943781b2db254937d56f681830580ca9
Bryan Vanegas <brayanxdvanegas@hotmail.com>
2023-02-20 15:48:59 -0500

    unificando cambios

--- src/controllers/Company.controller.js
+++ src/controllers/Company.controller.js
@@ -1,18 +1,49 @@
 import Company from "../models/Company.model.js";
-import GeneralQuerySql from "../DTO/GeneralQuerySql.js"
+import City from '../models/City.model.js';
+//Inserción de datos al modelo de "ProfileAccount"
+import Profile_account from "../models/Profile_account.model.js";
+
+import GeneralQuerySql from "../DTO/GeneralQuerySql.js";
 
 class CompanyController extends GeneralQuerySql {
 
+    //NO OLVIDAR asignar el async en el metodo al solucionar... GRACIAS!!.
+    static async Create(req, res) {
+        try {
 
-    static async Create(req,res){
-            try {
-                // const company = new Company(req.body)
-                // const data = await company.create()
-            } catch (error) {
-                res.status(500).json({
-                    message: error.message
-                });
+            const { name, nitOrLastname, email, date, password, description, city } = req.body;
+
+            const companyInfo = {
+                name: name,
+                NIT: nitOrLastname,
+                email: email,
+                password: password,
+                day_of_founded: date,
+                description: description,
+                type: "company",
+                img: req.file.filename,
             }
+
+            const user = new User(companyInfo)
+            await user.create()
+            companyInfo.id_user = await user.lastUser(user.email)
+
+            companyInfo.id_city = await City.idCity(city)
+
+            const company = new Company(companyInfo)
+            await company.create()
+            companyInfo.key = await Company.lastRegisterId()
+
+           const profile = new Profile_account(companyInfo)
+           await profile.create()
+
+            res.redirect('/')
+
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
     }
 
     static getAll = async (req, res) => {
