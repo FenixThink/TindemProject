3d947c48c9bd5922d0a35df944c2f2a58816e999
MorgaMcode <morgamcode@gmail.com>
2023-02-27 14:12:55 -0500

    boton y evento para brian

--- public/javascripts/components/chatViewComponents/all-chats/iconsButtons.js
+++ public/javascripts/components/chatViewComponents/all-chats/iconsButtons.js
@@ -9,8 +9,12 @@ export function iconsButtons(){
   const profileButton = buttonCreator();
   profileButton.className='profile-icon icons-buttons'
 
-  const chatButton = buttonCreator();
-  chatButton.className='chat-icon icons-buttons'
+  const SignOff = buttonCreator();
+  SignOff.className='SignOff-icon icons-buttons'
+
+  SignOff.addEventListener('click', () =>{
+    alert('UwU');
+  })
 
   //Creación de íconos
   const iconHome = document.createElement('i');
@@ -19,19 +23,20 @@ export function iconsButtons(){
   const iconProfile = document.createElement('i');
   iconProfile.className = 'fa fa-user';
 
-  const iconChat = document.createElement('i');
-  iconChat.className = 'fa fa-comment';
+  const iconChat = document.createElement('i'); 
+  iconChat.className = 'bx bxs-exit';
 
   //Agregar los íconos a los botones
   homeButton.appendChild(iconHome)
-  profileButton.appendChild(iconProfile)/* 
-  chatButton.appendChild(iconChat) */
+  profileButton.appendChild(iconProfile)
+  SignOff.appendChild(iconChat) 
 
   // Creción del div contenedor de los iconos
   const parent = document.createElement('div')
   parent.className = "parent-icons"
   parent.appendChild(homeButton)
   parent.appendChild(profileButton)
+  parent.appendChild(SignOff)
 
   return parent
 }
