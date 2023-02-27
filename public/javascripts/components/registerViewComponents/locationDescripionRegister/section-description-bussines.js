01bdff7d4c45b4dfeafaf2e521981fa06f79cc05
KDOE-117 <jdn2037@gmail.com>
2023-02-27 01:51:37 -0500

    Re-estructurando el merge con EA

--- public/javascripts/components/registerViewComponents/locationDescripionRegister/section-description-bussines.js
+++ public/javascripts/components/registerViewComponents/locationDescripionRegister/section-description-bussines.js
@@ -8,17 +8,24 @@ export function descriptionBussines(textLabelDescription) {
     const description = document.createElement('textarea');
     description.row = "10"
     description.col = "50"
-    description.placeholder = "Placeholder"
+    description.placeholder = "Descripción Breve"
     description.className = "description"
+    description.id = "descriptionID"
     description.name = "description"
-    description.maxLength = "280"
-
+    description.maxLength = "255"
+    description.required = true;
+    /*Creación de la etiqueta P referente al texto de error del campo Descrición */
+    const errorDescriptionField = document.createElement('p');
+    errorDescriptionField.id = "descriptionErrorID";
+    errorDescriptionField.className = "inputDescriptionError";
+    errorDescriptionField.textContent = "Puede digitar hasta 255 caracteres como máximo";
+    errorDescriptionField.hidden = true;
     //contenedor de fecha y ubicación
     const contDescription = document.createElement('div');
     contDescription.className = "container-description"
     contDescription.appendChild(descLabel);
     contDescription.appendChild(description);
-
+    contDescription.appendChild(errorDescriptionField);
     return contDescription;
 
 }
--- public/javascripts/components/registerViewComponents/parent.js
+++ public/javascripts/components/registerViewComponents/parent.js
@@ -9,29 +9,30 @@ import { AreasRoles } from "../modalOneCreateComponet/modalCreator.js";
 
 let areas = ["Ingenieria de sistemas", "Administracion de empresas", "Comunicacion  social", "Finanzas y negocios internacionales", "Mercadeo y publicidad", "Seguridad", "Servcios generales", "Medicina"]
 
-export const parentCreator = async (img, firstInput, secondInput, date, textLabelDescription, interestSpan,city,country,role) => {
+export const parentCreator = async (img, firstInput, secondInput, date, textLabelDescription, interestSpan, city, country, role) => {
 
     const top = principalFunction(img, firstInput, secondInput)
-    const midBot = await totalSectionB(date, textLabelDescription,city,country)
+    const midBot = await totalSectionB(date, textLabelDescription, city, country)
     const interest = contentDad(interestSpan)
     const button = buttonCreator()
     button.className = 'submitButton'
     button.textContent = 'Enviar'
-
-    /* button.type = 'button' */
     button.type = 'submit'
 
     const child = document.createElement('form')
     child.className = "Padre"
     child.name = "Padre"
+    child.id = "formCompany"
+    child.autocomplete = "off"
     //En Revisión
     child.action = `/${role}/create`
     child.enctype = 'multipart/form-data'
+    /* child.setAttribute('onsubmit', 'return validarFormulario()') */
     child.method = 'post'
     child.appendChild(top)
     child.appendChild(midBot)
     child.appendChild(interest)
-    child.appendChild( await AreasRoles(areas))
+    child.appendChild(await AreasRoles(areas))
     child.appendChild(button)
 
     const parent = document.createElement('div')
